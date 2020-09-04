e705025019139bb910469eeae1b5bdbb37d0e026
liyixuan <liyixuan4030614@gmail.com>
2020-09-04 01:57:18 -0400

    linked treenodes BST implementation.

--- assets/2020-09-02-Binary_Trees/src/linked_treenodes_based/LinkedTreeNodesBST.h
+++ assets/2020-09-02-Binary_Trees/src/linked_treenodes_based/LinkedTreeNodesBST.h
@@ -1,7 +1,16 @@
+#include <utility>
+
 #ifndef LINKEDLISTBST_H
 #define LINKEDLISTBST_H
 
+#include <algorithm>    // max
 #include <iostream>
+#include <stdexcept>
+#include <set>
+
+using std::cout;
+using std::endl;
+using std::set;
 
 template<typename T>
 class LinkedTreeNodesBST {
@@ -9,11 +18,13 @@ private:
     // Tree Node
     struct TreeNode {
         T data;
+        int id;     // same as index in ArrayBST
         TreeNode *leftChild;
         TreeNode *rightChild;
 
         // constructor
-        TreeNode(T data, TreeNode *left = nullptr, TreeNode *right = nullptr) : data {data}, leftChild {left}, rightChild {right} {
+        explicit TreeNode(T data, int id = 1, TreeNode *left = nullptr, TreeNode *right = nullptr)
+            : id {id}, data {data}, leftChild {left}, rightChild {right} {
         }
     };
 
@@ -46,39 +57,43 @@ public:
     //////////////////////////////////////////////////////////////////
 
     ////////////////////// Principle Operations //////////////////////
-    void insert(T value);
+    TreeNode* insertRecursive(TreeNode *root, int id, T value);
 
-    T remove(T value);
+    void insertIterative(TreeNode *root, T value);
 
-    TreeNode& searchByValue(T value);
+    TreeNode* remove(TreeNode *root, T value);
 
-    void preOrder(const TreeNode &) const;
+    TreeNode& searchByValueRecursive(TreeNode* root, T value);
 
-    void inOrder(const TreeNode &) const;
+    TreeNode& searchByValueIterative(TreeNode* root, T value);
 
-    void postOrder(const TreeNode &) const;
+    void preOrder(const TreeNode *root) const;
 
-    void levelOrder(const TreeNode &) const;
+    void inOrder(const TreeNode *root) const;
+
+    void postOrder(const TreeNode *root) const;
+
+    void levelOrder(const TreeNode *root) const;
 
     size_t getHeight() const;
 
-    TreeNode& getRoot() const;
+    TreeNode* getRoot() const;
     //////////////////////////////////////////////////////////////////
 
     ////////////////////// Auxiliary Operations //////////////////////
     bool isEmpty() const;
 
-    bool isBST(const TreeNode &) const;
+    bool isBST(const TreeNode &root, T minValue, T maxValue) const;
 
-    TreeNode& getParent(const TreeNode &) const;
+//    TreeNode& getParent(const TreeNode &node) const;
 
-    TreeNode& getLeftChild(const TreeNode &) const;
+//    TreeNode& getLeftChild(const TreeNode &node) const;
 
-    TreeNode& getRightChild(const TreeNode &) const;
+//    TreeNode& getRightChild(const TreeNode &node) const;
 
     size_t countNodes() const;
 
-    void destroyTree(TreeNode *);
+    void visualizeBST();
 
     //////////////////////////////////////////////////////////////////
 
@@ -86,10 +101,20 @@ public:
 private:
     TreeNode *root;
 
+    size_t count;
+
     /////////////////////// Auxiliary Function ///////////////////////
     bool isEqual(const TreeNode *, const TreeNode *);
 
     void deepcopy(TreeNode *, const TreeNode *);
+
+    void destroyTree(TreeNode *root);
+
+    TreeNode* findMinimumNode(TreeNode *root);
+
+    bool printLevel(const TreeNode *root, int level) const;
+
+    size_t calcHeight(const TreeNode *root) const;
 
     //////////////////////////////////////////////////////////////////
 };
@@ -97,16 +122,14 @@ private:
 /////////////////////// Function Implementation ///////////////////////
 // default constructor
 template<typename T>
-LinkedTreeNodesBST<T>::LinkedTreeNodesBST() : root{nullptr} {
+LinkedTreeNodesBST<T>::LinkedTreeNodesBST() : root{nullptr}, count{0} {
 }
 
 // constructor
 template<typename T>
 LinkedTreeNodesBST<T>::LinkedTreeNodesBST(T value) {
-    if (root != nullptr) {
-        destroyTree();
-    }
     root = new TreeNode(value);
+    count = 1;
 }
 
 // compare two BSTs
@@ -120,6 +143,7 @@ bool LinkedTreeNodesBST<T>::operator==(const LinkedTreeNodesBST<T> &llb) {
 template<typename T>
 LinkedTreeNodesBST<T>::~LinkedTreeNodesBST() {
     destroyTree(root);
+    count = 0;
 }
 
 // 2. copy constructor
@@ -129,8 +153,9 @@ LinkedTreeNodesBST<T>::LinkedTreeNodesBST(const LinkedTreeNodesBST &llb) {
         // destroy the current BST
         destroyTree(root);
     }
-    // copy the root node
+    // copy the root node and count
     root = new TreeNode(llb.root->data);
+    count = llb.count;
     // deepcopy llb's left and right subtrees
     deepcopy(root, llb.root);
 }
@@ -149,6 +174,7 @@ LinkedTreeNodesBST<T>& LinkedTreeNodesBST<T>::operator=(const LinkedTreeNodesBST
     }
     // copy the root node
     root = TreeNode(llb.root->data);
+    count = llb.count;
     // deepcopy llb's left and right subtrees
     deepcopy(root, llb.root);
 
@@ -164,6 +190,7 @@ LinkedTreeNodesBST<T>::LinkedTreeNodesBST(LinkedTreeNodesBST &&llb) noexcept {
     // steal everything from llb
     // for simplicity, just make root point to llb.root
     root = llb.root;
+    count = llb.count;
 
     // reset llb to stable states
     llb.root = nullptr;
@@ -191,24 +218,255 @@ LinkedTreeNodesBST<T>& LinkedTreeNodesBST<T>::operator=(LinkedTreeNodesBST &&llb
 }
 
 /////////////////////// Principle Operations ///////////////////////
+// insert (recursive approach)
 template<typename T>
-void LinkedTreeNodesBST<T>::insert(T value) {
-    // TODO: continue
+typename LinkedTreeNodesBST<T>::TreeNode* LinkedTreeNodesBST<T>::insertRecursive(TreeNode *root, int id, T value) {
+    if (root == nullptr) {
+        return new TreeNode(value, id);
+    } else if (root->data == value) {
+        // we assume all values in BST are unique
+        // so refuse to insert
+        throw std::runtime_error("duplicate value is inserted.");
+    } else if (root->data > value) {
+        // insert the value in the left subtree
+        root->leftChild = insertRecursive(root->leftChild, root->id * 2, value);
+    } else {
+        // insert the value in the right subtree
+        root->rightChild = insertRecursive(root->rightChild, root->id * 2 + 1, value);
+    }
+    return root;
 }
 
+// insert (iterative approach)
+template<typename T>
+void LinkedTreeNodesBST<T>::insertIterative(TreeNode *root, T value) {
+    // start with root node
+    TreeNode *curr = root;
+    // store parent node of current node
+    TreeNode *parent = nullptr;
+
+    // if current BST is empty
+    if (curr == nullptr) {
+        curr = new TreeNode(value);
+    }
+
+    // traverse the tree and find the right parent node
+    while (curr != nullptr) {
+        // update parent node as current node
+        parent = curr;
+
+        if (value < curr->data) {
+            // traverse current's left subtree if value < curr->value
+            curr = curr->leftChild;
+        } else {
+            // traverse current's right subtree if value > curr->value
+            curr = curr->rightChild;
+        }
+    }
+    // so far, we have found the right parent node whose the new value belongs
+
+    // create a new TreeNode and assign it to appropriate left/right pointer
+    if (value < parent->data) {
+        parent->leftChild = new TreeNode(value, parent->id * 2);
+        count++;
+    } else {
+        parent->rightChild = new TreeNode(value, parent->id * 2 + 1);
+        count++;
+    }
+}
+
+// remove node with certain value
+template<typename T>
+typename LinkedTreeNodesBST<T>::TreeNode* LinkedTreeNodesBST<T>::remove(LinkedTreeNodesBST<T>::TreeNode *root, T value) {
+    if (root == nullptr) {
+        // BST is empty
+        return nullptr;
+    }
+    if (value < root->data) {
+        // value should be in the root's left subtree
+        remove(root->leftChild, value);
+    } else if (value > root->data) {
+        // value should be in the root's right subtree
+        remove(root->rightChild, value);
+    } else {
+        // the node to be removed is found
+
+        if (root->leftChild == nullptr && root->rightChild == nullptr) {
+            // scenario 1: leaf node, the node has no children
+            delete root;    // wipe out the memory
+            root = nullptr;
+        } else if (root->leftChild != nullptr && root->rightChild == nullptr) {
+            // scenario 2.1: partial internal node with a left child
+            TreeNode* temp = root;
+            root = root->leftChild; // make the current pointer point to the left child
+            delete temp;    // wipe out the memory
+        } else if (root->leftChild == nullptr && root->rightChild != nullptr) {
+            // scenario 2.2: partial internal node with a right child
+            TreeNode *temp = root;
+            root = root->rightChild;
+            delete temp;
+        } else {
+            // scenario 3: complate internal node with two children
+            // step 1: find the minimum node in the right subtree
+            TreeNode* minimum = findMinimumNode(root->rightChild);
+            // step 2: replace the value of current node with the minimum value
+            root->data = minimum->data;
+            // step 3: remove the minimum node
+            root->rightChild = remove(root->rightChild, minimum->data);
+        }
+        count--;
+    }
+    return root;
+}
+
+// search TreeNode by value (recursive approach)
+template<typename T>
+typename LinkedTreeNodesBST<T>::TreeNode& LinkedTreeNodesBST<T>::searchByValueRecursive(TreeNode* root, T value) {
+    if (root->data == value) {
+        return root;
+    } else if (root->data > value) {
+        // search in the left subtree
+        return searchByValueRecursive(root->leftChild, value);
+    } else {
+        // search in the right subtree
+        return searchByValueRecursive(root->rightChild, value);
+    }
+}
+
+// search TreeNode by value (iterative approach)
+template<typename T>
+typename LinkedTreeNodesBST<T>::TreeNode& LinkedTreeNodesBST<T>::searchByValueIterative(TreeNode* root, T value) {
+    // keep reference to the current root
+    TreeNode* curr = root;
+    // traverse root's appropriate subtree
+    while (curr != nullptr) {
+        if (curr->data == value) {
+            return curr;
+        } else if (curr->data > value) {
+            curr = curr->leftChild;
+        } else {
+            curr = curr->rightChild;
+        }
+    }
+    return curr;
+}
+
+// four types of tree traversal: preOrder, inOrder, postOrder, levelOrder
+template<typename T>
+void LinkedTreeNodesBST<T>::preOrder(const LinkedTreeNodesBST<T>::TreeNode *root) const {
+    if (root == nullptr) {
+//        cout << "(_)";
+        return;
+    }
+
+    // visit and print the current node
+    cout << root->data << " ";
+//    cout << "(";
+
+    // visit left child
+    preOrder(root->leftChild);
+
+    // visit right child
+    preOrder(root->rightChild);
+
+//    cout << ")";
+}
+
+template<typename T>
+void LinkedTreeNodesBST<T>::inOrder(const LinkedTreeNodesBST<T>::TreeNode *root) const {
+    if (root == nullptr) {
+//        cout << "(_)";
+        return;
+    }
+
+//    cout << "(";
+
+    // visit left child first
+    inOrder(root->leftChild);
+
+    // visit and print the current node
+//    cout << ")";
+    cout << root->data << " ";
+//    cout << "(";
+
+    // visit right child
+    inOrder(root->rightChild);
+
+//    cout << ")";
+}
+
+template<typename T>
+void LinkedTreeNodesBST<T>::postOrder(const LinkedTreeNodesBST<T>::TreeNode *root) const {
+    if (root == nullptr) {
+//        cout << "(_)";
+        return;
+    }
+
+    // visit left child
+//    cout << "(";
+
+    postOrder(root->leftChild);
+
+//    cout << ")(";
+
+    // visit right child
+    postOrder(root->rightChild);
+
+//    cout << ")";
+
+    // visit and print the current node
+    cout << root->data << " ";
+}
+
+template<typename T>
+void LinkedTreeNodesBST<T>::levelOrder(const LinkedTreeNodesBST<T>::TreeNode *root) const {
+    if (root == nullptr) {
+        // empty tree
+        return;
+    }
+
+    // start from level 1
+    int level = 1;
+    while (printLevel(root, level)) {
+        level++;
+    }
+}
+
+// there are two approaches to find the height
+// approach 1: recursive calculate the height of left subtree and right subtree and select the max
+// approach 2: since the program records the id for each TreeNode, we can find the rightmost TreeNode at the deepest level, and calculate the height based on its id and the formula
+// the getHeight uses approach 1
+template<typename T>
+size_t LinkedTreeNodesBST<T>::getHeight() const {
+    return calcHeight(root);
+}
+
+template<typename T>
+typename LinkedTreeNodesBST<T>::TreeNode* LinkedTreeNodesBST<T>::getRoot() const {
+    return root;
+}
 
 /////////////////////// Auxiliary Operations ///////////////////////
 template<typename T>
-void LinkedTreeNodesBST<T>::destroyTree(TreeNode *root) {
-    if (root != nullptr) {
-        // delete its left and right subtrees
-        destroyTree(root->leftChild);
-        destroyTree(root->rightChild);
-        // and then delete itself
-        delete root;
-        // defensive programming
-        root = nullptr;
+bool LinkedTreeNodesBST<T>::isEmpty() const {
+    return root == nullptr;
+}
+
+template<typename T>
+bool LinkedTreeNodesBST<T>::isBST(const LinkedTreeNodesBST<T>::TreeNode &root, T minValue, T maxValue) const {
+    if (root == nullptr) {
+        return true;
     }
+    if (root.data < minValue || root.data > maxValue) {
+        return false;
+    }
+    return isBST(root.leftChild, minValue, root.data - 1)
+        && isBST(root.rightChild, root.data + 1, maxValue);
+}
+
+template<typename T>
+size_t LinkedTreeNodesBST<T>::countNodes() const {
+    return count;
 }
 
 /////////////////////// Auxiliary Functions ////////////////////////
@@ -249,6 +507,115 @@ void LinkedTreeNodesBST<T>::deepcopy(LinkedTreeNodesBST<T>::TreeNode *destRoot,
         // deepcopy srcRoot's right subtree
         deepcopy(destRoot->rightChild, srcRoot->rightChild);
     }
+}
+
+template<typename T>
+void LinkedTreeNodesBST<T>::destroyTree(TreeNode *root) {
+    if (root != nullptr) {
+        // delete its left and right subtrees
+        destroyTree(root->leftChild);
+        destroyTree(root->rightChild);
+        // and then delete itself
+        delete root;
+        // defensive programming
+        root = nullptr;
+    }
+}
+
+template<typename T>
+typename LinkedTreeNodesBST<T>::TreeNode * LinkedTreeNodesBST<T>::findMinimumNode(LinkedTreeNodesBST<T>::TreeNode *root) {
+    if (root == nullptr) {
+        return INT_MAX;
+    }
+    if (root->leftChild != nullptr) {
+        // the most left node is the minimum node
+        return findMinimumNode(root->leftChild);
+    } else {
+        return root;
+    }
+}
+
+template<typename T>
+bool LinkedTreeNodesBST<T>::printLevel(const LinkedTreeNodesBST<T>::TreeNode *root, int level) const {
+    if (root == nullptr) {
+        // reach to the end
+        return false;
+    }
+    if (level == 1) {
+        cout << root->data << " ";
+        return true;
+    }
+    bool left = printLevel(root->leftChild, level - 1);
+    bool right = printLevel(root->rightChild, level - 1);
+    return left || right;
+}
+
+template<typename T>
+size_t LinkedTreeNodesBST<T>::calcHeight(const LinkedTreeNodesBST<T>::TreeNode *root) const {
+    if (root == nullptr) {
+        // reach to the end
+        return 0;
+    } else {
+        size_t leftSubtreeHeight = calcHeight(root->leftChild);
+        size_t rightSubtreeHeight = calcHeight(root->rightChild);
+        return std::max(leftSubtreeHeight, rightSubtreeHeight) + 1;
+    }
+}
+
+template<typename T>
+void LinkedTreeNodesBST<T>::visualizeBST() {
+    // create a dummy class containing 4 functions for tree visualization
+    class dummy {
+    public:
+        dummy(set<int> s) : S{std::move(s)} {
+        }
+
+        void indent(int level) {
+            for (int i = 0; i < level; ++i) {
+                if (S.count(i) == 1) {
+                    cout << "| ";
+                } else {
+                    cout << " ";
+                }
+            }
+        }
+
+        // tee shape starts a vertical bar
+        void tee(int level) {
+            indent(level);
+            cout << "|-";
+            S.insert(level);
+        }
+
+        // ell shape ends one in the same column
+        void ell(int level) {
+            indent(level);
+            cout << "`-";
+            S.erase(level);
+        }
+
+        void print(TreeNode *node, int level) {
+            if (node == nullptr) {
+                // only print when node only has 1 child
+                cout << "[null]" << endl;
+            } else {
+                // print node
+                cout << "(" << node->data << ")" << endl;
+                // recursively print node's children
+                tee(level);
+                print(node->leftChild, level + 1);
+                ell(level);
+                print(node->rightChild, level + 1);
+            }
+        }
+
+    private:
+        // S tracks which columns need the vertical bars
+        set<int> S;
+    };
+
+    dummy treeVisulizer(std::set<int>{});
+    treeVisulizer.print(getRoot(), 1);
 }
 
 #endif //LINKEDLISTBST_H
--- assets/2020-09-02-Binary_Trees/src/linked_treenodes_based/main.cpp
+++ assets/2020-09-02-Binary_Trees/src/linked_treenodes_based/main.cpp
@@ -1,4 +1,90 @@
-//
-// Created by  liyixuan on 2020-09-03.
-//
+#include <iostream>
+#include <assert.h>
+#include "LinkedTreeNodesBST.h"
+
+using std::cout;
+using std::endl;
+
+const int rootId = 1;
+
+int main() {
+    // test default constructor
+    LinkedTreeNodesBST<int> ltnb_1;
+    assert(ltnb_1.countNodes() == 0);
+    assert(ltnb_1.isEmpty() == true);
+
+    // test constructor
+    LinkedTreeNodesBST<int> ltnb_2(20);
+    assert(ltnb_2.countNodes() == 1);
+    assert(ltnb_2.isEmpty() == false);
+
+    // test insertRecursive, insertIterative
+    // insert nodes in the left subtree
+    ltnb_2.insertRecursive(ltnb_2.getRoot(), rootId, 10);
+    ltnb_2.insertRecursive(ltnb_2.getRoot(), rootId, 5);
+    ltnb_2.insertRecursive(ltnb_2.getRoot(), rootId, 15);
+    ltnb_2.insertRecursive(ltnb_2.getRoot(), rootId, 1);
+    ltnb_2.insertRecursive(ltnb_2.getRoot(), rootId, 9);
+    ltnb_2.insertRecursive(ltnb_2.getRoot(), rootId, 7);
+    ltnb_2.insertRecursive(ltnb_2.getRoot(), rootId, 6);
+    ltnb_2.insertRecursive(ltnb_2.getRoot(), rootId, 8);
+    ltnb_2.insertRecursive(ltnb_2.getRoot(), rootId, 11);
+    ltnb_2.insertRecursive(ltnb_2.getRoot(), rootId, 18);
+    ltnb_2.insertRecursive(ltnb_2.getRoot(), rootId, 16);
+    ltnb_2.insertRecursive(ltnb_2.getRoot(), rootId, 17);
+    ltnb_2.insertRecursive(ltnb_2.getRoot(), rootId, 19);
+    // insert nodes in the right subtree
+    ltnb_2.insertIterative(ltnb_2.getRoot(), 30);
+    ltnb_2.insertIterative(ltnb_2.getRoot(), 25);
+    ltnb_2.insertIterative(ltnb_2.getRoot(), 35);
+    ltnb_2.insertIterative(ltnb_2.getRoot(), 21);
+    ltnb_2.insertIterative(ltnb_2.getRoot(), 29);
+    ltnb_2.insertIterative(ltnb_2.getRoot(), 26);
+    ltnb_2.insertIterative(ltnb_2.getRoot(), 27);
+    ltnb_2.insertIterative(ltnb_2.getRoot(), 28);
+    ltnb_2.insertIterative(ltnb_2.getRoot(), 31);
+    ltnb_2.insertIterative(ltnb_2.getRoot(), 39);
+    ltnb_2.insertIterative(ltnb_2.getRoot(), 38);
+    ltnb_2.insertIterative(ltnb_2.getRoot(), 41);
+    ltnb_2.insertIterative(ltnb_2.getRoot(), 40);
+    ltnb_2.visualizeBST();
+    cout << endl;
+
+    LinkedTreeNodesBST<int> ltnb_3(6);
+    ltnb_3.insertRecursive(ltnb_3.getRoot(), rootId, 3);
+    ltnb_3.insertRecursive(ltnb_3.getRoot(), rootId, 1);
+    ltnb_3.insertRecursive(ltnb_3.getRoot(), rootId, 0);
+    ltnb_3.insertRecursive(ltnb_3.getRoot(), rootId, 2);
+    ltnb_3.insertRecursive(ltnb_3.getRoot(), rootId, 4);
+    ltnb_3.insertRecursive(ltnb_3.getRoot(), rootId, 5);
+    ltnb_3.insertIterative(ltnb_3.getRoot(), 9);
+    ltnb_3.insertIterative(ltnb_3.getRoot(), 8);
+    ltnb_3.insertIterative(ltnb_3.getRoot(), 7);
+    ltnb_3.insertIterative(ltnb_3.getRoot(), 11);
+    ltnb_3.insertIterative(ltnb_3.getRoot(), 10);
+    ltnb_3.insertIterative(ltnb_3.getRoot(), 12);
+
+    // test preOrder, inOrder, postOrder, levelOrder
+    // expected result:
+    //   preOrder : 6 3 1 0 2 4 5 9 8 7 11 10 12
+    //    inOrder : 0 1 2 3 4 5 6 7 8 9 10 11 12
+    // postOrder  : 0 2 1 5 4 3 7 8 10 12 11 9 6
+    // levelOrder : 6 3 9 1 4 8 11 0 2 5 7 10 12
+    // (note: results above do not show any '(', ')', '_' characters)
+    cout << "  preOrder : ";
+    ltnb_3.preOrder(ltnb_3.getRoot());
+    cout << endl;
+    cout << "   inOrder : ";
+    ltnb_3.inOrder(ltnb_3.getRoot());
+    cout << endl;
+    cout << "postOrder  : ";
+    ltnb_3.postOrder(ltnb_3.getRoot());
+    cout << endl;
+    cout << "levelOrder : ";
+    ltnb_3.levelOrder(ltnb_3.getRoot());
+    cout << endl;
+
+
+    return 0;
+}
 
